dc4d050280a76b46fcceedb55157aa7eb6f81f38
Vũ Hào <hao326694@gmail.com>
2025-10-14 03:51:57 +0700

    Update url return in VNPayConfig

--- src/main/java/util/VNPayConfig.java
+++ src/main/java/util/VNPayConfig.java
@@ -4,5 +4,7 @@ public class VNPayConfig {
     public static final String vnp_TmnCode = "3W9851QV"; // Mã test sandbox chính thức
     public static final String vnp_HashSecret = "MSNR5K3O2B69ICIHBSAHZCUC458F4KU9"; // KHÔNG thêm khoảng trắng!
     public static final String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static final String vnp_ReturnUrl = "http://localhost:8080/TicketBooking/payment-return";
+//    public static final String vnp_ReturnUrl = "http://localhost:8080/TicketBooking/payment-return";
+    public static final String vnp_ReturnUrl = "https://tenfinity-cineverse.onrender.com/payment-return";
+
 }
